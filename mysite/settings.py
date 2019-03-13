0862b968868b688177eaf7c9bdd89efcb4662158
“Woonjang <ahnroonjang@likelion.org>
2019-03-13 16:11:49 +0900

    수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@3t+u@7l=@kcwn=il5pgr)*tlofznd%i1u&@bm%p&h7$_vf+=p'
+# SECRET_KEY = '@3t+u@7l=@kcwn=il5pgr)*tlofznd%i1u&@bm%p&h7$_vf+=p'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', '@3t+u@7l=@kcwn=il5pgr)*tlofznd%i1u&@bm%p&h7$_vf+=p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
